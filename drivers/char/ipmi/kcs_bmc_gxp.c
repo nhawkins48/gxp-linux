9f3ab104eb20637265f9cb9745e4c418486b17b7
Jorge Cisneros <jorge.cisneros@hpe.com>
2021-10-25 16:14:31 -0500

    Update driver to the new model and functions
    Signed-off-by: Jorge Cisneros <jorge.cisneros@hpe.com>

--- drivers/char/ipmi/kcs_bmc_gxp.c
+++ drivers/char/ipmi/kcs_bmc_gxp.c
@@ -21,7 +21,8 @@
 #include <linux/slab.h>
 #include <linux/timer.h>
 
-#include "kcs_bmc.h"
+#include "kcs_bmc_device.h"
+
 
 #define DEVICE_NAME     "gxp-kcs-bmc"
 
@@ -37,54 +38,76 @@
 
 #define GXP_KCS_LPC_ACTIVATE  0x01
 
-struct hpe_kcs_bmc {
+struct gxp_kcs_bmc {
+
+	struct kcs_bmc_device kcs_bmc;
+
 	struct resource *kcsResource;
 	void __iomem *kcsRegMap;
 	struct regmap *kcsCfgMap;
 };
 
-static u8 hpe_kcs_inb(struct kcs_bmc *kcs_bmc, u32 reg)
+static inline struct gxp_kcs_bmc *to_gxp_kcs_bmc(struct kcs_bmc_device *kcs_bmc)
 {
-	struct hpe_kcs_bmc *priv = kcs_bmc_priv(kcs_bmc);
+	return container_of(kcs_bmc, struct gxp_kcs_bmc, kcs_bmc);
+}
+
+static u8 hpe_kcs_inb(struct kcs_bmc_device *kcs_bmc, u32 reg)
+{
+	struct gxp_kcs_bmc *priv = to_gxp_kcs_bmc(kcs_bmc);
 	u32 val = 0;
 
 	val = readb(priv->kcsRegMap + reg);
 	return (u8) val;
 }
 
-static void hpe_kcs_outb(struct kcs_bmc *kcs_bmc, u32 reg, u8 data)
+static void hpe_kcs_outb(struct kcs_bmc_device *kcs_bmc, u32 reg, u8 data)
 {
-	struct hpe_kcs_bmc *priv = kcs_bmc_priv(kcs_bmc);
+	struct gxp_kcs_bmc *priv = to_gxp_kcs_bmc(kcs_bmc);
 
 	writeb(data, priv->kcsRegMap + reg);
 }
 
-static void hpe_kcs_turn_on_auto_irq(struct kcs_bmc *kcs_bmc)
+static void hpe_kcs_updateb(struct kcs_bmc_device *kcs_bmc, u32 reg, u8 mask, u8 val)
 {
-	struct hpe_kcs_bmc *priv = kcs_bmc_priv(kcs_bmc);
+        struct gxp_kcs_bmc *priv = to_gxp_kcs_bmc(kcs_bmc);
+        int rc;
+
+        rc = regmap_update_bits(priv->kcsRegMap, reg, mask, val);
+        WARN(rc != 0, "regmap_update_bits() failed: %d\n", rc);
+}
+
+
+static void hpe_kcs_turn_on_auto_irq(struct kcs_bmc_device *kcs_bmc)
+{
+	struct gxp_kcs_bmc *priv = to_gxp_kcs_bmc(kcs_bmc);
 
 	writeb(GXP_KCS_AUTOIRQ | GXP_KCS_MSKOBFINT,
 		priv->kcsRegMap + GXP_LPC_INT);
 }
 
-static void hpe_kcs_activate(struct kcs_bmc *kcs_bmc)
+static void hpe_kcs_activate(struct kcs_bmc_device *kcs_bmc)
 {
-	struct hpe_kcs_bmc *priv = kcs_bmc_priv(kcs_bmc);
+	struct gxp_kcs_bmc *priv = to_gxp_kcs_bmc(kcs_bmc);
 
 	regmap_write(priv->kcsCfgMap, 0x00, GXP_KCS_LPC_ACTIVATE);
 }
 
+static void hpe_kcs_irq_mask_update(struct kcs_bmc_device *kcs_bmc, u8 mask, u8 state)
+{
+	pr_debug("%s Update mask %x state %x\n", __func__, mask, state);
+}
 
 static irqreturn_t hpe_kcs_irq(int irq, void *arg)
 {
-	struct kcs_bmc *kcs_bmc = arg;
+	struct kcs_bmc_device *kcs_bmc = arg;
 
 	if (!kcs_bmc_handle_event(kcs_bmc))
 		return IRQ_HANDLED;
 	return IRQ_NONE;
 }
 
-static int hpe_kcs_config_irq(struct kcs_bmc *kcs_bmc,
+static int hpe_kcs_config_irq(struct kcs_bmc_device *kcs_bmc,
 	    struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
@@ -104,65 +127,74 @@ static const struct kcs_ioreg gxp_kcs_bmc_ioregs[KCS_CHANNEL_MAX] = {
 	{ .idr = GXP_LPC_IDR, .odr = GXP_LPC_ODR, .str = GXP_LPC_STR },
 };
 
+static const struct kcs_bmc_device_ops hpe_kcs_ops = {
+	.irq_mask_update = hpe_kcs_irq_mask_update,
+	.io_inputb = hpe_kcs_inb,
+	.io_outputb = hpe_kcs_outb,
+	.io_updateb = hpe_kcs_updateb,
+};
+
+
 static int hpe_kcs_probe(struct platform_device *pdev)
 {
-	struct device *dev = &pdev->dev;
-	struct hpe_kcs_bmc *priv;
-	struct kcs_bmc *kcs_bmc;
+	struct gxp_kcs_bmc *priv;
+	struct kcs_bmc_device *kcs_bmc;
 	u32 chan;
 	int rc;
 
 	// kcs_chan => kcs channel
-	rc = of_property_read_u32(dev->of_node, "kcs_chan", &chan);
+	rc = of_property_read_u32(pdev->dev.of_node, "kcs_chan", &chan);
 	if ((rc != 0) || (chan == 0 || chan > KCS_CHANNEL_MAX)) {
-		dev_err(dev, "no valid 'kcs_chan' configured\n");
+		dev_err(&pdev->dev, "no valid 'kcs_chan' configured\n");
 		return -ENODEV;
 	}
 
-	kcs_bmc = kcs_bmc_alloc(dev, sizeof(*priv), chan);
-	if (!kcs_bmc) {
-		dev_err(dev, "Memory Allocate Fail\n");
+	priv = devm_kzalloc(&pdev->dev, sizeof(*priv), GFP_KERNEL);
+	if (!priv)
 		return -ENOMEM;
-	}
 
-	priv = kcs_bmc_priv(kcs_bmc);
+
+	kcs_bmc = &priv->kcs_bmc;
+	kcs_bmc->dev = &pdev->dev;
+	kcs_bmc->channel = chan;
+	kcs_bmc->ioreg = gxp_kcs_bmc_ioregs[chan - 1];
+	kcs_bmc->ops = &hpe_kcs_ops;
+
+	spin_lock_init(&kcs_bmc->lock);
 
 	priv->kcsResource = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	priv->kcsRegMap = devm_ioremap_resource(&pdev->dev, priv->kcsResource);
 	if (IS_ERR(priv->kcsRegMap))
 		return PTR_ERR(priv->kcsRegMap);
 
-	priv->kcsCfgMap = syscon_regmap_lookup_by_phandle(dev->of_node,
+	priv->kcsCfgMap = syscon_regmap_lookup_by_phandle(pdev->dev.of_node,
 							"kcs-bmc-cfg");
 	if (IS_ERR(priv->kcsCfgMap)) {
-		dev_err(dev, "Couldn't get KCS configuration regmap\n");
+		dev_err(&pdev->dev, "Couldn't get KCS configuration regmap\n");
 		return -ENODEV;
 	}
 
-	kcs_bmc->ioreg = gxp_kcs_bmc_ioregs[chan - 1];
-	kcs_bmc->io_inputb = hpe_kcs_inb;
-	kcs_bmc->io_outputb = hpe_kcs_outb;
 
-	dev_set_drvdata(dev, kcs_bmc);
+	platform_set_drvdata(pdev, priv);
 
 	rc = hpe_kcs_config_irq(kcs_bmc, pdev);
 	if (rc) {
-		dev_err(dev, "Fail to register IRQ - %d\n", rc);
+		dev_err(&pdev->dev, "Fail to register IRQ - %d\n", rc);
 		return rc;
 	}
 
-	rc = misc_register(&kcs_bmc->miscdev);
+	rc = kcs_bmc_add_device(&priv->kcs_bmc);
 	if (rc) {
-		dev_err(dev, "Unable to register device\n");
+		dev_err(&pdev->dev, "Unable to register device\n");
 		return rc;
 	}
 
 	// Turn on auto-IRQ to host and only interrupt IOP when IBF is set.
-	dev_info(dev, "Turn on Auto IRQ\n");
+	dev_info(&pdev->dev, "Turn on Auto IRQ\n");
 	hpe_kcs_turn_on_auto_irq(kcs_bmc);
 
 	// Turn on KCS0
-	dev_info(dev, "Activate KCS Device\n");
+	dev_info(&pdev->dev, "Activate KCS Device\n");
 	hpe_kcs_activate(kcs_bmc);
 
 	pr_info("channel=%u idr=0x%x odr=0x%x str=0x%x\n",
@@ -174,9 +206,10 @@ static int hpe_kcs_probe(struct platform_device *pdev)
 
 static int hpe_kcs_remove(struct platform_device *pdev)
 {
-	struct kcs_bmc *kcs_bmc = dev_get_drvdata(&pdev->dev);
+	struct gxp_kcs_bmc *priv = platform_get_drvdata(pdev);
+	struct kcs_bmc_device *kcs_bmc = &priv->kcs_bmc;
 
-	misc_deregister(&kcs_bmc->miscdev);
+	kcs_bmc_remove_device(kcs_bmc);
 
 	return 0;
 }
@@ -198,4 +231,5 @@ static struct platform_driver gxp_kcs_bmc_driver = {
 module_platform_driver(gxp_kcs_bmc_driver);
 
 MODULE_AUTHOR("John Chung <john.chung@hpe.com>");
+MODULE_AUTHOR("Jorge Cisneros <jorge.cisneros@hpe.com>");
 MODULE_DESCRIPTION("HPE device interface to the KCS BMC device");
